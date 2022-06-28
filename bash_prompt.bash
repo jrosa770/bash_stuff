cc41de70078d4b12d365add407af77e9b00f789f
Jose Rosa <jose770@gmail.com>
2022-06-28 10:46:20 -0400

    Update bash_prompt.bash

--- bash_prompt.bash
+++ bash_prompt.bash
@@ -5,7 +5,7 @@ git_branch() {
     git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 # TERMINAL PROMP
-PS1="\[\e[0;93m\]\\[\e[m\]@\h"    # username
+PS1="\[\e[0;93m\]\u\[\e[m\]@\h"    # username
 PS1+=" "    # spac
 PS1+="(\[\e[0;95m\]\W\[\e[m\])"    # current directory
 PS1+="\[\e[0;92m\]\$(git_branch)\[\e[m\]"    # current branch
